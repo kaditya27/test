03341c31dbcc1c1cf307ab975c2a552ca35ae858
kaditya27 <47591389+kaditya27@users.noreply.github.com>
2019-02-20 14:55:23 +0530

    Update main.c

--- main.c
+++ main.c
@@ -3,18 +3,41 @@
 
 int main()
 {
-    int sw;
-    printf("press 1 for switch is on and 2 for switch is off \n");
-    scanf("%d",&sw);
-    if(sw==1)
+
+    int inp[30],i,n;
+    int rise=0;
+    int fall=0;
+    int constant=0;
+    n=7;
+    inp[30]=1010101;
+
+    for(i=1;i<n;i++)
     {
-        inten();
+
+
+    if(inp[i]<inp[i+1])
+    {
+
+        rise++;
 
     }
-    else if(sw==2)
+
+    else if(inp[i]>inp[i+1])
     {
 
-        printf("wiper is turned off \n");
+        fall++;
+
     }
+    else if(inp[i]==inp[i+1])
+    {
+
+        constant++;
+
+    }
+
+    }
+    printf("the number of rising edges are %d \n",rise);
+    printf("the number of falling  edges are %d \n",fall);
+    printf("the number of constants are %d \n",constant);
     return 0;
 }
